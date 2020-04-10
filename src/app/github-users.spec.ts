55b34775134f3906b5418c569167368dbcf0bfee
Lucas Owino <lucowish35@gmail.com>
2020-04-10 14:24:17 +0300

    added components and service for app

---
+++ src/app/github-users.spec.ts
@@ -0,0 +1,7 @@
+import { GithubUsers } from './github-users';
+
+describe('GithubUsers', () => {
+  it('should create an instance', () => {
+    expect(new GithubUsers()).toBeTruthy();
+  });
+});
---
+++ src/app/github-users.ts
@@ -0,0 +1,3 @@
+export class GithubUsers {
+    userName: string
+}
