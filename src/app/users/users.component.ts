41279b5d92b589047b939b54c5089c880ab6a466
Lucas Owino <lucowish35@gmail.com>
2020-04-11 21:14:33 +0300

    Add logic to repo class and component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, } from '@angular/core';
 import { GithubUsers } from "../github-users";
 import { GithubService } from "../service/github/github.service";
-import { error } from 'protractor';
+import { GithubRepos } from "../github-repos";
 
 @Component({
   selector: 'app-users',
@@ -12,9 +12,10 @@ export class UsersComponent implements OnInit {
   searchUsers = true;
 
   githubUsers: GithubUsers;
+  githubRepos: GithubRepos;
   userName: string;
 
-  constructor(private githubUserServive: GithubService) { }
+  constructor(private githubUserServive: GithubService, private githubRepoServive: GithubService) { }
 
   search(userName) {
     this.githubUserServive.getUser(userName).then((success) => {
@@ -23,6 +24,13 @@ export class UsersComponent implements OnInit {
       (error) => {
         alert("username not found");
         console.log(error)
+      })
+
+    this.githubRepoServive.getRepo(userName).then((results) => {
+      this.githubRepos = this.githubRepoServive.updateRepo;
+    },
+      (error) => {
+        console.log(error);
       })
   }
 
