027c8cd0782a31a47fa6aa045fa45ebf39a3331f
Lucas Owino <lucowish35@gmail.com>
2020-04-11 19:47:53 +0300

    Add access_token to environment.ts file that was added to git ignore file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,5 @@
 export const environment = {
   production: false,
   access_token: 'd98d801a17a7fc591e0947f3831ed6f082e30622',
-  url: 'https://api.github.com/users/${access_token}',
-  url1: 'https://api.github.com/repositories/${access_token}'
 
 };
