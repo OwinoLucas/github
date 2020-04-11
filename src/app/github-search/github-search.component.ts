02743a56ae73fd4a4919fe275bb39a5b0acba666
Lucas Owino <lucowish35@gmail.com>
2020-04-11 19:32:54 +0300

    defined the github-search function in .ts file

--- src/app/github-search/github-search.component.ts
+++ src/app/github-search/github-search.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { GithubUsers } from "../github-users";
-import { GithubService } from '../service/github/github.service';
-import { environment } from "../../environments/environment";
-import { Observable } from "rxjs";
-import { promise } from 'protractor';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-github-search',
@@ -11,61 +6,23 @@ import { promise } from 'protractor';
   styleUrls: ['./github-search.component.css']
 })
 export class GithubSearchComponent implements OnInit {
-  @Input() githubUsers: GithubUsers
-  @Output() userUpdated: EventEmitter<GithubUsers> = new EventEmitter<GithubUsers>();
+  userName: string
+  @Output() userUpdated = new EventEmitter<any>();
 
 
-  constructor(private githubService: GithubService) { }
+  constructor() { }
 
   ngOnInit() {
-    if (this.githubUsers) {
-      this.githubUsers.userName = 'invalid'
-      this.getUserInfo();
-    }
-    // this.githubService.getUser()
-    // this.githubUsers = this.githubService.githubUsers
 
+  }
+  //the search fx for the event emitter
+  search() {
+    this.userUpdated.emit(this.userName);
+    this.userName = '';
   }
 
 
 
-  searchUser() {
-    if (this.githubUsers.userName && this.githubUsers.userName.length > 0) {
-      this.githubService.updateUser(this.githubUsers.userName);
-      this.getUserInfo();
-    }
-    else {
-      this.githubUsers.userName = 'invalid';
-    }
-
-  }
-
-  getUserInfo(): Observable<GithubUsers> {
-    if (this.githubUsers.userName && this.githubUsers.userName.length > 0) {
-      var out = this.githubService.getUser().subscribe((userName) => {
-        this.githubUsers = userName
-
-      }) as HTMLElement
-
-    }
-
-
-
-  }
-
-  // let promise = new Promise((resolve, reject) => {
-  // this.http.get<GitResponse>(`${environment.url}`).toPromise().then(response => {
-  //   this.githubUsers.userName = response.userName
-
-  //   resolve()
-  // },
-  //   error => {
-  //     this.githubUsers.userName = 'Enter valid user name'
-
-  //    reject(error)
-  //   })
-  // })
-  // return promise
 }
 
 
--- src/app/github-users.ts
+++ src/app/github-users.ts
@@ -1,3 +1,15 @@
 export class GithubUsers {
-    userName: string
+
+    constructor(
+        public login: string,
+        public avatar_url: string,
+        public location: string,
+        public html_url: string,
+        public name: string,
+        public email: string,
+        public public_repos: number,
+        public followers: number,
+        public following: number,
+        public created_at: Date
+    ) { }
 }
