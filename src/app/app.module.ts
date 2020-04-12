3ff744d514245e5f9f4d9e6ef44ca4270efc17c1
Lucas Owino <lucowish35@gmail.com>
2020-04-12 15:46:36 +0300

    Add loader to template for better user experience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { NgProgressModule } from "@ngx-progressbar/core";
+import { NgProgressHttpClientModule } from "@ngx-progressbar/http-client";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +23,9 @@ import { ReposComponent } from './repos/repos.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [GithubService],
   bootstrap: [AppComponent]
